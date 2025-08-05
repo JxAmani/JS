263bf5ca603b5166f788d320a29f794305434373
JxAmani <jeremyamani336@gmail.com>
2025-08-05 18:43:47 +0300

    Solution-1

---
+++ 01-firstNonRepeatingChar.js
@@ -0,0 +1,27 @@
+function firstNonRepeatingChar(string) {
+    const charCount = {};//store character counts
+
+    // Count occurrences
+    for (let char of string) {
+        if (charCount[char]) {
+            charCount[char]++;//if character appears more than once there will be an increament of the charCount
+        } else {
+            charCount[char] = 1;
+        }
+    }
+
+    // Find the first unique character
+    for (let char of string) {//to loop through the given string
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    // No unique characters found
+    return null;
+}
+
+// Test cases
+console.log(firstNonRepeatingChar("Hello")); // H
+console.log(firstNonRepeatingChar("Swiss")); // w
+console.log(firstNonRepeatingChar("aabb"));  // null
