8dca8d89f5f02bc64686c686716de01d5be927f5
JxAmani <jeremyamani336@gmail.com>
2025-08-06 09:27:02 +0300

    Solution to Q2

--- Q2/02-find-missing-number.js
+++ Q2/02-find-missing-number.js
@@ -1,3 +1,11 @@
+// Given a list containing n distinct numbers from 0 to n, find the missing number.
+
+
+
+// Input: [3, 0, 1]
+
+// Output: 2
+// NOTE: No number limit
 function findMissingNumber(numbers) {
     const n = numbers.length; // array length
 
